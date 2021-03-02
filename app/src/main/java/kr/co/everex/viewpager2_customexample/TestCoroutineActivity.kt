1258b21909bb0b44f42e3b4fd3959e379fc504ad
hunderboy <wheeing@naver.com>
2021-03-02 13:37:43 +0900

    // jBCrypt 를 통한 비밀번호 암호화 OK

--- app/src/main/java/kr/co/everex/viewpager2_customexample/TestCoroutineActivity.kt
+++ app/src/main/java/kr/co/everex/viewpager2_customexample/TestCoroutineActivity.kt
@@ -3,14 +3,21 @@ package kr.co.everex.viewpager2_customexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_test_coroutine.*
 import kotlinx.coroutines.*
 import kr.co.everex.viewpager2_customexample.databinding.ActivityTestCoroutineBinding
+import org.mindrot.jbcrypt.BCrypt
 
 class TestCoroutineActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTestCoroutineBinding
 
 
-//    private val job =
+    private var password: String? = null
+    private var passwordHashed: String? = null
+
+    private var confirmPassword: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +26,24 @@ class TestCoroutineActivity : AppCompatActivity() {
         setContentView(view) // 뷰 바인딩 적용 완료
 
 
-//        val job = launch {
-//            repeat(1000){ i ->`
-//                Log.e("TestCoroutineActivity :","")
-//                delay(500L)
-//            }
-//        }
-//        delay(500L)
+        binding.buttonCrypt.setOnClickListener{
+            password = binding.editTextTextPassword.text.toString()
+            passwordHashed = BCrypt.hashpw(password, BCrypt.gensalt(10))
+            Toast.makeText( baseContext, "암호화됨 : $passwordHashed", Toast.LENGTH_SHORT).show()
+        }
 
+        binding.buttonLogin.setOnClickListener{
+            confirmPassword = binding.editTextTextPasswordForLogin.text.toString()
 
+            val isValidPassword = BCrypt.checkpw(confirmPassword, passwordHashed)
+
+            if (isValidPassword){
+                Toast.makeText( baseContext, "비밀번호 일치함", Toast.LENGTH_SHORT).show()
+            } else{
+                Toast.makeText( baseContext, "PW not correct", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
 }
